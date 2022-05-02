ed4f6978709b267812740848a5052cfca40700d9
unknown <mortson@hotmail.co.uk>
2022-05-02 14:09:25 +0100

    Continued Header Positioning and Sizing Control Development

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let userNameFull = "Joshua Morton";
 let userName = "klavelon";
+let uNList = [];
 //let imgSrc = "url('images/userPicture.jpg')";
 
-document.querySelector('#userNameFull').textContent = userNameFull;
+/*Set all Full UserName Class Elements to the name set above.*/
+uNList = document.querySelectorAll('.userNameFull');
+for (i = uNList.length; i > 0; i--){
+    uNList[i-1].textContent = userNameFull;
+}
+
 document.querySelector('#userName').textContent = "(@" + userName + ")";
